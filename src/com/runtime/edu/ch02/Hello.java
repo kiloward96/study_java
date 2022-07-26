e760b0452ebb943ba64620c2ef3a02ceb0ff675d
kiloward96 <kiloward96@gmail.com>
2022-07-26 17:01:12 +0900

    commit

---
+++ src/com/runtime/edu/ch02/Hello.java
@@ -0,0 +1,49 @@
+package com.runtime.edu.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+
+		int width = 100;
+
+		String name = "LGG";
+
+		// 상세화면 변수
+
+		String product_name = "서핑 데크(대)";
+		double rate = 4.5;
+		int price = 431300;
+		String color = "색상 선택";
+		String option = "옵션 선택";
+		int stock = 1;
+		int total_price = 431300;
+		String model_name = "AQU_3095";
+		String size_weight = "상품 상세 확인";
+		String color_all = "yellow orange blue";
+		String merterial = "폴리에스터 포함";
+		String product_composition = "단일상품";
+		String release_date = "2021-01";
+		String company = "MORAY";
+		String contry = "중국";
+		String product_spec = "사이즈";
+		String product_quality = "소비자보호법 준수";
+
+		System.out.println(name);
+		System.out.println("name 변수에 들어가있는 데이터 : " + name);
+		
+		
+		System.out.println(product_name);
+		
+		System.out.println(product_name + rate + price + color + option + stock + total_price + model_name + size_weight
+				+ color_all + merterial + product_composition + release_date + company + contry + product_spec + product_quality);
+		
+		
+		
+		
+	}
+
+}
